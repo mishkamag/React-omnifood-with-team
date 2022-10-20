b374f97033b2919fd8db02a475475501ea1434e0
mishkamag <m93.maglaperidze@gmail.com>
2022-10-21 00:19:50 +0400

    add meals feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MainHeader from "./components/MainHeader";
 import Features from "./components/FeaturesSection/Features/Features";
+import Products from "./components/Productss/Products";
 function App() {
   return (
     <div>
       <MainHeader />
       <Features />
+      <Products />
     </div>
   );
 }
