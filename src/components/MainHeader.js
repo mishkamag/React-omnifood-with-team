7e16b3daf1841f586419ecf83ba6a3fc1bd1bb08
mishkamag <m93.maglaperidze@gmail.com>
2022-10-20 23:04:08 +0400

    react omnifood with mainHeader

---
+++ src/components/MainHeader.js
@@ -0,0 +1,43 @@
+import classes from "./MainHeader.module.css";
+import logoImg from "./images/logo-white.png";
+
+const MainHeader = () => {
+  return (
+    <header>
+      <nav className={classes.formNav}>
+        <a href="#">
+          <img className={classes.logo} src={logoImg} alt="logo picture" />
+        </a>
+        <ul className={classes.mainNav}>
+          <li>
+            <a href="#">FOOD DELIVERY </a>
+          </li>
+          <li>
+            <a href="#">HOW IT WORKS </a>
+          </li>
+          <li>
+            <a href="#">OUR CITIES </a>
+          </li>
+          <li>
+            <a href="#">SIGN UP</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={classes.heroText}>
+        <h1>
+          Goodbye junk food.
+          <br />
+          Hello super healthy meals
+        </h1>
+        <a href="#" className={classes.fullBtn}>
+          I’m hungry
+        </a>
+        <a href="#" className={classes.ghostBtn}>
+          Show me more
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;
